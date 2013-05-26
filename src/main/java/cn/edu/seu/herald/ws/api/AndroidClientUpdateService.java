cc34f0a631d523e77042e3fda8a23d4e64b48b6a
Wenhao Ji <predator.ray@gmail.com>
2013-05-26 21:07:55 +0800

    添加服务接口的返回类型

--- src/main/java/cn/edu/seu/herald/ws/api/AndroidClientUpdateService.java
+++ src/main/java/cn/edu/seu/herald/ws/api/AndroidClientUpdateService.java
@@ -1,9 +1,11 @@
 package cn.edu.seu.herald.ws.api;
 
+import cn.edu.seu.herald.ws.api.update.Update;
+
 /**
  * Copyright (c) 2013 Ray <predator.ray@gmail.com>
  */
 public interface AndroidClientUpdateService {
 
-    Object getNewVersion();
+    Update getNewVersion();
 }
--- src/main/java/cn/edu/seu/herald/ws/api/GradePointService.java
+++ src/main/java/cn/edu/seu/herald/ws/api/GradePointService.java
@@ -1,9 +1,11 @@
 package cn.edu.seu.herald.ws.api;
 
+import cn.edu.seu.herald.ws.api.gpa.GradePoint;
+
 /**
  * Copyright (c) 2013 Ray <predator.ray@gmail.com>
  */
 public interface GradePointService {
 
-    Object getGradePoint(String username, String password);
+    GradePoint getGradePoint(String username, String password);
 }
